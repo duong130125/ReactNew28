9d449a1c8b0ac1a5dbd7f38cf23b416e9287c855
duong130125 <nguyendinhduong2005yt@gmail.com>
2024-06-19 12:01:12 +0700

    ss68

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import "./Table.css";
+import "./Main.css";
 
 export default function Main() {
   const listBooks: any = useSelector((state) => state);
